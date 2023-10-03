7258268d504a1adaf3993f003d6f75026fee1a6b
Alejandro8011 <100472228@alumnos.uc3m.es>
2023-10-03 13:38:59 +0200

    Ejemplo lectura

---
+++ read_binary/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+
+int main() {
+    using namespace std;
+    const string archivoEntrada = "../files/small.fld";
+    ifstream archivo(archivoEntrada, ios::binary);
+
+    if (!archivo) {
+        cerr << "No se pudo abrir el archivo." << endl;
+        return 1;
+    }
+
+    char buffer[4];
+    int i = 0;
+    while (archivo.read(buffer, sizeof(buffer))&& (i < 28))  {
+        float data_simple;
+        memcpy(&data_simple, buffer, sizeof(data_simple));
+        double data = data_simple;
+        cout << "Número leído " << i << ": " << data << endl;
+        i++;
+    }
+    archivo.close();
+    return 0;
+}
