a586af6e8ffca46d4f88ac0eb551fd46372c3e32
Enrique Córdoba <100472146@alumnos.uc3m.es>
2023-10-15 12:32:11 +0200

    Bloque

--- sim/block.cpp
+++ sim/block.cpp
@@ -1,3 +1,33 @@
 //
 // Created by Kry0m on 03/10/2023.
 //
+
+#include <iostream>
+#include "block.hpp"
+
+Block::Block(
+    int n_particulas, std::vector<int> posicion,
+    std::vector<double> tamano,
+    std::vector<int> adyacentes
+    ) : n_particulas(n_particulas), posicion(std::move(posicion)),
+    tamano(std::move(tamano)),
+    bloques_adyacentes(std::move(adyacentes)) {
+
+}
+
+int Block::how_many_particles() const{
+  return n_particulas;
+}
+
+std::vector<int> Block::PosicionActual() const{
+  return posicion;
+}
+
+std::vector<double> Block::Dimensiones() const{
+  return tamano;
+}
+
+std::vector<int> Block::BloquesVecinos() const{
+  return bloques_adyacentes;
+}
+
--- sim/block.hpp
+++ sim/block.hpp
@@ -2,7 +2,43 @@
 // Created by Kry0m on 03/10/2023.
 //
 
-#ifndef PROYECTO_FLUIDOS_ARQUITECTURA_BLOCK_HPP
-#define PROYECTO_FLUIDOS_ARQUITECTURA_BLOCK_HPP
+#ifndef BLOCK_HPP
+#define BLOCK_HPP
+#include <iostream>
+#include <vector>
+#include <array>
+
+
+//typedef std::array<int, 8> adyacentes;
+
+/*
+struct Vector_adyacencias {
+    std::vector<int> lista_adyacencias
+
+};
+*/
+const int diecisiete = 17;
+
+class Block {
+  private:
+    int n_particulas;
+    std::vector<int> posicion;
+    std::vector<double> tamano;
+    std::vector<int> bloques_adyacentes;
+
+  public:
+    Block(
+        int n_particulas = 0,
+        std::vector<int> posicion = std::vector<int>(3, 0),
+        std::vector<double> tamano = std::vector<double>(3, 0.0),
+        std::vector<int> lista_adyacencias = std::vector<int>(diecisiete, -1)
+    );
+
+    [[nodiscard]] int how_many_particles () const;
+    [[nodiscard]] std::vector<int> PosicionActual() const;
+    [[nodiscard]] std::vector<double> Dimensiones() const;
+    [[nodiscard]] std::vector<int> BloquesVecinos() const;
+
+};
 
 #endif //PROYECTO_FLUIDOS_ARQUITECTURA_BLOCK_HPP
